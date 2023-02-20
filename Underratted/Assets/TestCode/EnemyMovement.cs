6d9c5ce4e0c2094e807c300200f57f9a2988300e
Sintija Krasiļņikova <2002959@uad.ac.uk>
2023-02-20 10:28:40 +0000

    Testing enemy movement, player and enemy animations
    Working out enemy movement, basic 'fallow player'-'return to start' enemy routine.

--- Underratted/Assets/TestCode/EnemyMovement.cs
+++ Underratted/Assets/TestCode/EnemyMovement.cs
@@ -11,18 +11,23 @@ public class EnemyMovement : MonoBehaviour
 
     public Rigidbody rigidBody;
     public float enemyRadius = 5f;
-    public float attackDistance = 1f;
 
+    //distance at which enemy stops chasing the player and goes for an attack
+    public float safeDistance = 3f;
+   // private Vector3 safeDistanceVector = new Vector3(safeDistance, 0, safeDistance);
 
     public Transform target;
     private Vector3 enemyMoveDirection;
     private Vector3 startPosition;
     public bool isHunting = false;
 
+    public Animator enemyAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.position;
+        enemyAnimator.SetBool("Hunting", isHunting);
     }
 
     // Update is called once per frame
@@ -32,14 +37,23 @@ public class EnemyMovement : MonoBehaviour
         {
 
             // OR for a square range, AND for a circle
-            if (Mathf.Abs(target.position.x + attackDistance - startPosition.x) < enemyRadius && Mathf.Abs(target.position.z + attackDistance - startPosition.z) < enemyRadius)
+            if (Mathf.Abs(target.position.x - startPosition.x) < enemyRadius && Mathf.Abs(target.position.z - startPosition.z) < enemyRadius)
             {
-                Vector3 direction = (target.position - transform.position).normalized;
-                Vector3 enemyToPlayer = new Vector3(direction.x, 0, direction.z);
-                
-                EnemyMove(enemyToPlayer);
+                //if (Mathf.Abs(target.position.x - safeDistance - startPosition.x) < enemyRadius && Mathf.Abs(target.position.z - safeDistance - startPosition.z) < enemyRadius)
+                //{
+                //    isHunting = false;
+                //    enemyAnimator.SetBool("Hunting", isHunting);
+                //}
+                //else
+                //{
+                    Vector3 direction = (target.position - transform.position).normalized;
+                    Vector3 enemyToPlayer = new Vector3(direction.x, 0, direction.z);
 
-                isHunting = true;
+                    EnemyMove(enemyToPlayer);
+
+                    isHunting = true;
+                    enemyAnimator.SetBool("Hunting", isHunting);
+                //}
             }
             else
             {
@@ -48,8 +62,11 @@ public class EnemyMovement : MonoBehaviour
 
                 EnemyMove(enemyToStart);
                 isHunting = false;
+                enemyAnimator.SetBool("Hunting", isHunting);
             }
         }
+
+        
     }
 
     private void EnemyMove(Vector3 currentDirection)
@@ -57,6 +74,62 @@ public class EnemyMovement : MonoBehaviour
         if (target != null)
         {
             rigidBody.velocity = currentDirection * enemyMoveSpeed * 100 * Time.deltaTime;
+
+
+            //if (currentDirection.sqrMagnitude >= 0.01)
+            //{
+                //left right for animation
+                //if (currentDirection.x < 0)// && Input.GetAxis("Vertical") ==0)
+                if (currentDirection.x < 0 && currentDirection.x < currentDirection.z)// && Input.GetAxis("Vertical") ==0)
+                {
+                    enemyAnimator.SetFloat("FacingUp", 0);
+                    enemyAnimator.SetFloat("FacingRight", -1); //left
+                }
+                else if (currentDirection.x <  0 && currentDirection.x > currentDirection.z)
+                {
+                    enemyAnimator.SetFloat("FacingRight", 0);
+                    enemyAnimator.SetFloat("FacingUp", -1); //down
+                 }
+
+                //up down for animation
+                if (currentDirection.x > 0 && currentDirection.x > currentDirection.z)// && Input.GetAxis("Horizontal") == 0)
+                {
+                //enemyAnimator.SetFloat("FacingRight", 0);
+                //enemyAnimator.SetFloat("FacingUp", -1); //down
+
+                    enemyAnimator.SetFloat("FacingUp", 0);
+                    enemyAnimator.SetFloat("FacingRight", 1); //right
+                 }
+                else if (currentDirection.x > 0 && currentDirection.x < currentDirection.z)
+                {
+                    enemyAnimator.SetFloat("FacingRight", 0);
+                    enemyAnimator.SetFloat("FacingUp", 1); //up
+                }
+
+
+            //if (currentDirection.x < 0)// && Input.GetAxis("Vertical") ==0)
+            //{
+            //    enemyAnimator.SetFloat("FacingUp", 0);
+            //    enemyAnimator.SetFloat("FacingRight", -1); //left
+            //}
+            //    else if (currentDirection.x > 0)
+            //{
+            //    enemyAnimator.SetFloat("FacingUp", 0);
+            //    enemyAnimator.SetFloat("FacingRight", 1); //right
+            //}
+
+            ////up down for animation
+            //if (currentDirection.z < 0)// && Input.GetAxis("Horizontal") == 0)
+            //{
+            //    enemyAnimator.SetFloat("FacingRight", 0);
+            //    enemyAnimator.SetFloat("FacingUp", -1); //down
+            //}
+            //else if (currentDirection.z > 0)
+            //{
+            //    enemyAnimator.SetFloat("FacingRight", 0);
+            //    enemyAnimator.SetFloat("FacingUp", 1); //up
+            //}
+            //// }
         }
     }
 
--- Underratted/Assets/TestCode/characterMovementScript.cs
+++ Underratted/Assets/TestCode/characterMovementScript.cs
@@ -14,6 +14,12 @@ public class characterMovementScript : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float gravity = 9.81f;
 
+    
+
+    private bool facingUp;
+    private bool facingLeft;
+
+
     public Animator plyerAnimator;
 
     // Start is called before the first frame update
@@ -31,6 +37,35 @@ public class characterMovementScript : MonoBehaviour
         plyerAnimator.SetFloat("Horizontal", playerMovementInput.x);
         plyerAnimator.SetFloat("Vertical", playerMovementInput.z);
         plyerAnimator.SetFloat("Speed", playerMovementInput.sqrMagnitude);
+
+
+        if (playerMovementInput.sqrMagnitude >= 0.01)
+        {
+            //left right for animation
+            if (Input.GetAxis("Horizontal") < 0)// && Input.GetAxis("Vertical") ==0)
+            {
+                plyerAnimator.SetFloat("FacingUp", 0); 
+                plyerAnimator.SetFloat("FacingRight", -1); //left
+            }
+            else if (Input.GetAxis("Horizontal") > 0)
+            {
+                plyerAnimator.SetFloat("FacingUp", 0); 
+                plyerAnimator.SetFloat("FacingRight", 1); //right
+            }
+
+            //up down for animation
+            if (Input.GetAxis("Vertical") < 0)// && Input.GetAxis("Horizontal") == 0)
+            {
+                plyerAnimator.SetFloat("FacingRight", 0);
+                plyerAnimator.SetFloat("FacingUp", -1); //down
+            }
+            else if(Input.GetAxis("Vertical") > 0)
+            {
+                plyerAnimator.SetFloat("FacingRight", 0);
+                plyerAnimator.SetFloat("FacingUp", 1); //up
+            }
+        }
+
     }
 
     private void MovePlayer()
